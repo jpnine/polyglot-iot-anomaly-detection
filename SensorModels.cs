672c52d9cc68c8d531589a8e1ce8c19e5bbe2e4c
Jaimin <jaimin.ece@gmail.com>
2025-12-27 23:38:32 -0500

    Add project files.

---
+++ SensorModels.cs
@@ -0,0 +1,8 @@
+﻿namespace SensorApi
+{
+    public class SensorModels
+    {
+        public record SensorReading(string MachineId, double Temperature, DateTime Datetime);
+        public record AnomalyResult(bool Anomaly);
+    }
+}
